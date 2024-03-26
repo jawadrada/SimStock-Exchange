716f596e19132cce8ba12e6dfcf8f56f69a39bba
Adefela Fakorode <adefakorode@gmail.com>
2024-03-26 19:44:01 -0400

    init react+vite n tailwind.css

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Welcome to SimStock-Exchange!</h1>
+      <p className="text-xl text-blue-700 font-bold">coming soon...</p>
+    </>
+  )
+}
+
+export default App
---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  screens: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+  },
+}
